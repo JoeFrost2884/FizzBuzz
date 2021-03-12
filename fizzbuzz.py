b727463d21522cd6267d299a1e16a2d7a6971b0c
Joseph Collins <josephcollins@centurytel.net>
2021-03-12 13:27:42 -0600

    Main Code cleanup

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for num in range(1,101): # for loop to go through number range 1 - 101
-    if num % 5 == 0 and num % 3 == 0: # if statement to check if number is  divisible by 3 and 5
+for number in range(1,101): # for loop to go through number range 1 - 101
+    if number % 5 == 0 and num % 3 == 0: # if statement to check if number is  divisible by 3 and 5
         print("FizzBuzz") # print FizzBuzz if statement is true
-    elif num % 3 == 0:   # else if statement to check if number is divisible by 3 with a remainder of 0
+    elif number % 3 == 0:   # else if statement to check if number is divisible by 3 with a remainder of 0
         print("Fizz")   # print Fizz if statement is true
-    elif num % 5 == 0:   # else if statement to check if number is divisible by 5 with a remainder of 0
+    elif number % 5 == 0:   # else if statement to check if number is divisible by 5 with a remainder of 0
         print("Buzz")   # print Buzz if statement is true
     else:
-        print(num)# print after all statements are false
+        print(number)# print after if all above statements are false
