9573237f8fa2293017dfa2ac2818fc1e5451879e
Joseph Collins <josephcollins@centurytel.net>
2021-03-12 12:38:18 -0600

    Version 1 of FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,9 @@
-#for number in range(1,101):
-#    if number % 3 == 0 and number % 5 == 0:
-#        print("FizzBuzz")
-#        continue
-#    elif number % 3 == 0:
-#        print("Fizz")
-#        continue
-#    elif number % 5 == 0:
-#        print("Buzz")
-#        continue
-#    print(number)
-
-for num in range(1,101):
-    string = ""
-    if num % 3 == 0:
-        string = string + "Fizz"
-    if num % 5 == 0:
-        string = string + "Buzz"
-    if num % 5 != 0 and num % 3 != 0:
-        string = string + str(num)
-    print(string)
+for num in range(1,101): # for loop to go through number range 1 - 101
+    containerStr = ""  # empty container string
+    if num % 3 == 0:   # if statement to check if number is divisible by 3 with a remainder of 0
+        containerStr = containerStr + "Fizz"   # put Fizz into string if statement is true
+    if num % 5 == 0:   # if statement to check if number is divisible by 5 with a remainder of 0
+        containerStr = containerStr + "Buzz"   # put Buzz into string if statement is true
+    if num % 5 != 0 and num % 3 != 0: # if statement to check if number is not divisible by 3 and 5
+        containerStr = containerStr + str(num) # print number into string if statement is true
+    print(containerStr) # print string after after a statement is true
