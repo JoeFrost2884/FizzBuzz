bb3a58b4752bd21d43c51d3c3b9fa561bac49d22
Joseph Collins <josephcollins@centurytel.net>
2021-03-12 12:22:40 -0600

    branch1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,21 @@
-print("Hello")
+#for number in range(1,101):
+#    if number % 3 == 0 and number % 5 == 0:
+#        print("FizzBuzz")
+#        continue
+#    elif number % 3 == 0:
+#        print("Fizz")
+#        continue
+#    elif number % 5 == 0:
+#        print("Buzz")
+#        continue
+#    print(number)
+
+for num in range(1,101):
+    string = ""
+    if num % 3 == 0:
+        string = string + "Fizz"
+    if num % 5 == 0:
+        string = string + "Buzz"
+    if num % 5 != 0 and num % 3 != 0:
+        string = string + str(num)
+    print(string)
